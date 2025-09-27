1ee670f8a2d9b51c88c2357060ad1bda674be99d
Keval M Mahadik <129382407+Keval-M-Mahadik@users.noreply.github.com>
2025-09-27 21:09:17 +0530

    Add files via upload
    Signed-off-by: Keval M Mahadik <129382407+Keval-M-Mahadik@users.noreply.github.com>

--- find/mian 1.py
+++ find/mian 1.py
@@ -1,22 +1,20 @@
 import requests
 
-def request(url):
+def request(url, output_file):
     try:
-        ty=requests.get("http://" + url)
-        if(result):
-            print("[+] subdomin  discovered ---->" + url)
-    except:
+        response = requests.get("https://" + url)
+        if response.status_code == 200:
+            print("[+] subdomain discovered ----> " + url)
+            output_file.write(url + "\n")
+    except requests.RequestException:
         pass
 
-
 def main():
-    tc = "googlo.com"
-
-    with open ("text.txt" , "r") as wordlist:
+    tc = "youtube.com"
+    with open("text.txt", "r") as wordlist, open("domain.txt", "a") as output_file:
         for line in wordlist:
             word = line.strip()
-            test_url = word + "." + tc 
-            request(test_url)
+            test_url = word + "." + tc
+            request(test_url, output_file)
 
 main()
-
